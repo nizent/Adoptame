fa505bfd3f88f53d96afdab4f10ad40c4bfdcbfe
nizent <43146103+nizent@users.noreply.github.com>
2021-04-30 16:14:32 -0400

    Create zage.js

---
+++ zage.js
@@ -0,0 +1,11 @@
+var myStorage = window.localStorage, pageCount;
+window.addEventListener('load', function(){
+   if(!myStorage.getItem("pageCount")){
+      myStorage.setItem('pageCount', 1);
+   } else {
+      pageCount = myStorage.getItem("pageCount");
+      pageCount = pageCount + 1;
+      myStorage.setItem('pageCount', pageCount );
+   }
+   console.log('page view count', myStorage.getItem("pageCount"));
+});
